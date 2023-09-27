0cb1d002bfe57a802bb5eb21bed7684cebbf0fe9
pg <p.m.genest@gmail.com>
2023-09-28 01:35:59 +0200

    all kinda scuffed

--- shutdown_query.py
+++ shutdown_query.py
@@ -11,24 +11,36 @@ def check_who():
 
     names = [line.split(None, 1)[0] for line in r.splitlines()]
     if len(names) > 0:
-        print(f"Someone is using the server: {names}")
+        print(f"The following users are on the server: {names}")
         return True
     else:
         return False
 
 
-def check_factorio(path):
+def check_factorio(name):
+    path = "/home/camilo/" + name + "/factorio/factorio-current.log"
+    subprocess.run(["/usr/bin/screen", "-S", name, "-X", "stuff", "/players\n"])
+    time.sleep(0.1)
     with open(path, 'r') as file:
-        data = json.load(file)
-    res = data.get("players")
-    if res == None:
-        return False
-    if len(res) > 0:
-        print(f"There are {res} players online in factorio.")
-        return True
+        line_list = list(file)
+        line_list.reverse()
+        #last_line = file.readlines()[-1]
+        r = 0
+        for line in line_list:
+            print(line)
+            if "/players" in line:
+                break
+            if "(online)" in line:
+                r += 1
+        if r > 0:
+            print(f"There are {r} playrs online in factorio.")
+            return True
+        else:
+            return False
+
 
 def check_minecraft(path):
-    subprocess.run(["/usr/bin/screen", "-S", "minecraft", "-X", "stuff", '"/list^M"'])
+    subprocess.run(["/usr/bin/screen", "-S", "minecraft", "-X", "stuff", "/list\n"])
     time.sleep(0.1)
     with open(path, 'r') as file:
         last_line = file.readlines()[-1]
@@ -36,10 +48,12 @@ def check_minecraft(path):
     end_index = last_line.find("of", start_index)
     if start_index != -1 and end_index != -1:
         resmc = last_line[start_index:end_index].strip()
-        print("Extracted value:", resmc)
+        # print("Extracted value:", resmc)
     else:
-        print("Substring not found in the string.")
+        print("MC substring error")
+        return True
     if int(resmc) > 0:
+        print(f"There are {res} players online in minecraft")
         return True
     else:
         return False
@@ -48,24 +62,27 @@ def checkAll():
     global funclist
     r = False
     for f in funclist:
-        r = r or f()
+        t = f()
+        r = r or t
     return r
 
 if __name__ == "__main__":
     global funclist
     funclist = []
-    """
-    funclist.append(lambda: check_who())
-    funclist.append(lambda: check_factorio("/home/camilo/factorio_cse/factorio/player-data.json"))
-    funclist.append(lambda: check_factorio("/home/camilo/factorio_adrian/factorio/player-data.json"))
-    funclist.append(lambda: check_minecraft("/home/camilo/minecraft/logs/latest.log"))
 
+    funclist.append(lambda: check_who())
+    funclist.append(lambda: check_factorio("factorio_cse"))
+    funclist.append(lambda: check_factorio("factorio_adrian"))
+    funclist.append(lambda: check_minecraft("/home/camilo/minecraft/logs/latest.log"))
+    print(checkAll())
+"""
     if(not checkAll()):
         time.sleep(DELAY*60)
         if(not checkAll()):
             subprocess.run(["/bin/bash", "/home/camilo/utils/shutdown.sh"])
-    """
-    
-    check_who()
-    check_factorio("player-data.json")
-    check_minecraft("latest.log")
+"""    
+    #check_who()
+    #check_factorio("/home/camilo/factorio_adrian/factorio/player-data.json")
+    #check_minecraft("/home/camilo/minecraft/logs/latest.log")
+    #check_factorio("player-data.json")
+    #check_minecraft("latest.log")
