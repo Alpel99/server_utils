e2d411e905d9555cd39763786f5d342132abf1ed
pg <p.m.genest@gmail.com>
2023-10-03 22:14:39 +0200

    startup for status

--- startup.sh
+++ startup.sh
@@ -2,5 +2,6 @@
 screen -d -m -S minecraft bash -c '$HOME/utils/startmc.sh'
 screen -d -m -S factorio_cse bash -c '$HOME/utils/startfactorio_cse.sh'
 screen -d -m -S factorio_adrian bash -c '$HOME/utils/startfactorio_adrian.sh'
+screen -d -m -S server_status bash -c '/usr/bin/python3 $HOME/utils/server_status.py'
 # screen -d -m -S filebrowser bash -c '$HOME/filebrowser/run.sh'
 echo "started mc+fc servers"
